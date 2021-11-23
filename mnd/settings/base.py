995bfc5384666836a17e8cca6b71d41c736afa75
Dexter <dexedge@gmail.com>
2021-11-22 22:50:04 -0500

    Tweak base.py

--- mnd/settings/base.py
+++ mnd/settings/base.py
@@ -160,7 +160,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/3.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
