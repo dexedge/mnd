bb7015e7e23eefec8ccfc230c0192727b03d8826
Dexter <dexedge@gmail.com>
2021-11-23 14:58:48 -0500

    Add sentry

--- mnd/settings/production.py
+++ mnd/settings/production.py
@@ -29,3 +29,20 @@ COMPRESS_CSS_FILTERS = [
     'compressor.filters.cssmin.CSSMinFilter',
 ]
 COMPRESS_CSS_HASHING_METHOD = 'content'
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://809729f029d142059b17d910dfd31317@o1076423.ingest.sentry.io/6078237",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
