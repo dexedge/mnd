51897dd9fd5e0b38ed312b055c59d2014271327b
Dexter <dexedge@gmail.com>
2021-11-23 11:46:07 -0500

    Tweak wsgi.py

--- mnd/wsgi.py
+++ mnd/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mnd.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mnd.settings.production")
 
 application = get_wsgi_application()
