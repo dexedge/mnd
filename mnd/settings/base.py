aad1c7782ff003bf875811af725111d828cd2bb3
Dexter <dexedge@gmail.com>
2021-11-22 19:36:41 -0500

    Change backend to Postgres

--- mnd/settings/base.py
+++ mnd/settings/base.py
@@ -99,6 +99,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mnd',
+        'USER': 'Dexter',
+        'PASSWORD': 'moz1756',
+        'HOST': 'localhost',
+        'PORT': '',
+        
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
