700c05b33debb7cff7186ab553d95335c64b72b9
Dexter <dexedge@gmail.com>
2021-11-22 21:59:52 -0500

    Tweaks

--- mnd/settings/base.py
+++ mnd/settings/base.py
@@ -66,7 +66,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     
-
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
 
@@ -161,7 +160,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/3.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
