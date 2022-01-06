04c85391b0763c8dc3a456b4b3668b2ce1198695
Dexter <dexedge@gmail.com>
2022-01-06 16:31:03 -0500

    Remove search results pagination for now

--- search/views.py
+++ search/views.py
@@ -20,19 +20,22 @@ def search(request):
 
         # Record hit
         query.add_hit()
+
     # else:
     #     search_results = Page.objects.none()
 
     # Pagination
-    paginator = Paginator(search_results, 10)
-    try:
-        search_results = paginator.page(page)
-    except PageNotAnInteger:
-        search_results = paginator.page(1)
-    except EmptyPage:
-        search_results = paginator.page(paginator.num_pages)
+    # count_results = search_results
+    # paginator = Paginator(search_results, 10)
+    # try:
+    #     search_results = paginator.page(page)
+    # except PageNotAnInteger:
+    #     search_results = paginator.page(1)
+    # except EmptyPage:
+    #     search_results = paginator.page(paginator.num_pages)
 
     return TemplateResponse(request, 'search/search.html', {
         'search_query': search_query,
         'search_results': search_results,
+        # 'count_results': count_results,
     })
