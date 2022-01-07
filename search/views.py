3f7e80e4926c27933c851c5b8ce7b0c0b7d4ae43
Dexter <dexedge@gmail.com>
2022-01-07 14:18:38 -0500

    Style search results page

--- search/views.py
+++ search/views.py
@@ -8,34 +8,33 @@ from documents.models import DocumentPage
 
 
 def search(request):
-    search_query = request.GET.get('query', None)
+    search_query = request.GET.get('query', '')
     page = request.GET.get('page', 1)
 
-    search_results = DocumentPage.objects.live()
     # Search
+    search_results = DocumentPage.objects.live()
     if search_query:
-        search_results = list(search_results.search(search_query))
-        search_results.sort(key=lambda x: x.date)
+        search_results = search_results.search(search_query, order_by_relevance=False)
         query = Query.get(search_query)
 
         # Record hit
         query.add_hit()
-
     # else:
-    #     search_results = Page.objects.none()
+        # search_results = Page.objects.none()
+        
 
     # Pagination
-    # count_results = search_results
-    # paginator = Paginator(search_results, 10)
-    # try:
-    #     search_results = paginator.page(page)
-    # except PageNotAnInteger:
-    #     search_results = paginator.page(1)
-    # except EmptyPage:
-    #     search_results = paginator.page(paginator.num_pages)
+    count_results = search_results.count
+    paginator = Paginator(search_results, 10)
+    try:
+        search_results = paginator.page(page)
+    except PageNotAnInteger:
+        search_results = paginator.page(1)
+    except EmptyPage:
+        search_results = paginator.page(paginator.num_pages)
 
     return TemplateResponse(request, 'search/search.html', {
         'search_query': search_query,
         'search_results': search_results,
-        # 'count_results': count_results,
+        'count_results': count_results,
     })
