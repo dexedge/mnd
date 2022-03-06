cd5e0dcae8328acbdbfc521b67f0601a5b7c8068
Dexter <dexedge@gmail.com>
2022-03-06 10:01:32 -0500

    Refine image formats

--- home/image_formats.py
+++ home/image_formats.py
@@ -1,9 +1,9 @@
 from wagtail.images.formats import Format, register_image_format
 
-register_image_format(Format("small", "Small Right", "richtext-image right", "max-400x400"))
+register_image_format(Format("small", "Small Right", "richtext-image small-right", "max-400x400"))
 
-register_image_format(Format("smallleft", "Small Left", "richtext-image left", "max-400x400"))
+register_image_format(Format("smallleft", "Small Left", "richtext-image small-left", "max-400x400"))
 
-register_image_format(Format("smallcenter", "Small Center", "richtext-image smallcenter", "max-400x400"))
+register_image_format(Format("smallcenter", "Small Center", "richtext-image small-center", "max-400x400"))
 
-register_image_format(Format("mediumcenter", "Medium Center", "richtext-image center", "max-600x600"))
+register_image_format(Format("mediumcenter", "Medium Center", "richtext-image medium-center", "max-600x600"))
