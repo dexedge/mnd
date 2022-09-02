f598a546b646ada313c8bf46f05fa1fbd4ad7a91
Dexter <dexedge@gmail.com>
2022-09-02 12:00:56 -0400

    Increase DATA_UPLOAD_MAX_NUMBER_FIELDS

--- mnd/settings/base.py
+++ mnd/settings/base.py
@@ -147,6 +147,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 5000
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
