b6c3e58ccf4ef71b10f9124e28e8252c026d3284
Dexter <dexedge@gmail.com>
2023-08-13 15:49:25 -0400

    Update search backend in base.py

--- mnd/settings/base.py
+++ mnd/settings/base.py
@@ -195,7 +195,7 @@ WAGTAIL_SITE_NAME = "mnd"
 
 WAGTAILSEARCH_BACKENDS = {
     'default': {
-        'BACKEND': 'wagtail.contrib.postgres_search.backend',
+        'BACKEND': 'wagtail.search.backends.database',
     },
 }
 
