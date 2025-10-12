f2d32136fe7303b7b68b17e433caf462fc1db16b
Dexter <dexedge@gmail.com>
2025-10-12 10:40:24 -0400

    Remove AWS ACL in settings

--- mnd/settings/base.py
+++ mnd/settings/base.py
@@ -176,9 +176,6 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
 
-# Set the default ACL for uploaded files to 'public-read'
-AWS_DEFAULT_ACL = 'public-read'
-
 AWS_QUERYSTRING_AUTH = False 
 # This will make sure that the file URL does not have unnecessary parameters like your access key.
 AWS_S3_FILE_OVERWRITE = False
